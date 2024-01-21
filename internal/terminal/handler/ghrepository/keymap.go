cdbaac6a080281cd468d4eba0ce3fd47548f488e
Açıkgöz Engin <engin.acikgoz@protonmail.com>
2024-01-21 17:05:34 +0300

    Add search feature for repositories

--- internal/terminal/handler/ghrepository/keymap.go
+++ internal/terminal/handler/ghrepository/keymap.go
@@ -5,20 +5,20 @@ import (
 )
 
 type keyMap struct {
-	Refresh   teakey.Binding
-	LaunchTab teakey.Binding
-	TabSwitch teakey.Binding
+	Refresh      teakey.Binding
+	LaunchOption teakey.Binding
+	TabSwitch    teakey.Binding
 }
 
 func (k keyMap) ShortHelp() []teakey.Binding {
-	return []teakey.Binding{k.TabSwitch, k.Refresh, k.LaunchTab}
+	return []teakey.Binding{k.TabSwitch, k.Refresh, k.LaunchOption}
 }
 
 func (k keyMap) FullHelp() [][]teakey.Binding {
 	return [][]teakey.Binding{
 		{k.TabSwitch},
 		{k.Refresh},
-		{k.LaunchTab},
+		{k.LaunchOption},
 	}
 }
 
@@ -27,7 +27,7 @@ var keys = keyMap{
 		teakey.WithKeys("r", "R"),
 		teakey.WithHelp("r/R", "Refresh list"),
 	),
-	LaunchTab: teakey.NewBinding(
+	LaunchOption: teakey.NewBinding(
 		teakey.WithKeys("enter"),
 		teakey.WithHelp("enter", "Launch the selected option"),
 	),
--- internal/terminal/handler/types/types.go
+++ internal/terminal/handler/types/types.go
@@ -1,7 +1,6 @@
 package types
 
 type SelectedRepository struct {
-	RepositoryID   int64  // repository id
 	RepositoryName string // full repository name (owner/name)
 	WorkflowName   string // workflow name
 	BranchName     string // branch name
