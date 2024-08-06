ca9658a8d1eec88eca535f8c927ced578ec680bc
Engin <engin.acikgoz@protonmail.com>
2024-08-07 01:05:53 +0300

    remove unused code

--- internal/terminal/style/style.go
+++ internal/terminal/style/style.go
@@ -5,21 +5,17 @@ import (
 )
 
 var (
-	DocStyle          = lipgloss.NewStyle().Padding(1, 2, 1, 2)
 	WindowStyleOrange = lipgloss.NewStyle().BorderForeground(lipgloss.Color("#ffaf00")).Border(lipgloss.RoundedBorder())
 	WindowStyleRed    = lipgloss.NewStyle().BorderForeground(lipgloss.Color("9")).Border(lipgloss.RoundedBorder())
 	WindowStyleGreen  = lipgloss.NewStyle().BorderForeground(lipgloss.Color("10")).Border(lipgloss.RoundedBorder())
 	WindowStyleGray   = lipgloss.NewStyle().BorderForeground(lipgloss.Color("240")).Border(lipgloss.NormalBorder())
 	WindowStyleWhite  = lipgloss.NewStyle().BorderForeground(lipgloss.Color("255")).Border(lipgloss.NormalBorder())
-	WindowStyleYellow = lipgloss.NewStyle().BorderForeground(lipgloss.Color("11")).Border(lipgloss.NormalBorder())
-	WindowStylePink   = lipgloss.NewStyle().BorderForeground(lipgloss.Color("205")).Border(lipgloss.RoundedBorder())
 
-	WindowStyleHelp           = WindowStyleGray.Margin(0, 0, 0, 1).Padding(0, 2, 0, 2).Border(lipgloss.RoundedBorder())
-	WindowStyleError          = WindowStyleRed.Margin(0, 0, 0, 1).Padding(0, 2, 0, 2).Border(lipgloss.RoundedBorder())
-	WindowStyleProgress       = WindowStyleOrange.Margin(0, 0, 0, 1).Padding(0, 2, 0, 2).Border(lipgloss.RoundedBorder())
-	WindowStyleSuccess        = WindowStyleGreen.Margin(0, 0, 0, 1).Padding(0, 2, 0, 2).Border(lipgloss.RoundedBorder())
-	WindowStyleDefault        = WindowStyleWhite.Margin(0, 0, 0, 1).Padding(0, 2, 0, 2).Border(lipgloss.RoundedBorder())
-	WindowStyleOptionSelector = WindowStylePink.Margin(0, 0, 0, 1).Padding(0, 2, 0, 2).Border(lipgloss.RoundedBorder())
+	WindowStyleHelp     = WindowStyleGray.Margin(0, 0, 0, 1).Padding(0, 2, 0, 2).Border(lipgloss.RoundedBorder())
+	WindowStyleError    = WindowStyleRed.Margin(0, 0, 0, 1).Padding(0, 2, 0, 2).Border(lipgloss.RoundedBorder())
+	WindowStyleProgress = WindowStyleOrange.Margin(0, 0, 0, 1).Padding(0, 2, 0, 2).Border(lipgloss.RoundedBorder())
+	WindowStyleSuccess  = WindowStyleGreen.Margin(0, 0, 0, 1).Padding(0, 2, 0, 2).Border(lipgloss.RoundedBorder())
+	WindowStyleDefault  = WindowStyleWhite.Margin(0, 0, 0, 1).Padding(0, 2, 0, 2).Border(lipgloss.RoundedBorder())
 )
 
 var (
@@ -42,19 +38,5 @@ var (
 		b.Right = "├"
 		b.Left = "┤"
 		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 2).BorderForeground(lipgloss.Color("240")).Foreground(lipgloss.Color("240"))
-	}()
-
-	TitleStyleLiveModeOn = func() lipgloss.Style {
-		b := lipgloss.DoubleBorder()
-		b.Right = "├"
-		b.Left = "┤"
-		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 2).BorderForeground(lipgloss.Color("#aa1010")).Foreground(lipgloss.Color("#ff0000"))
-	}()
-
-	TitleStyleLiveModeOff = func() lipgloss.Style {
-		b := lipgloss.DoubleBorder()
-		b.Right = "├"
-		b.Left = "┤"
-		return lipgloss.NewStyle().BorderStyle(b).Padding(0, 2).BorderForeground(lipgloss.Color("#101010"))
 	}()
 )
