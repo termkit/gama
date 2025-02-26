e183326e26eb60b4fa3ef23dc67ed32114ea6f01
atilsensalduz <atil.sensalduz@gmail.com>
2025-02-26 10:19:08 +0300

    fix: address security warnings from gosec scanner
    - Handle error returned by viper.BindEnv() to fix G104 (CWE-703)
    - Implement URL validation in browser.OpenInBrowser() to prevent command
      injection vulnerability G204 (CWE-78)
    
    Signed-off-by: atilsensalduz <atil.sensalduz@gmail.com>

--- pkg/browser/browser.go
+++ pkg/browser/browser.go
@@ -1,24 +1,34 @@
 package browser
 
 import (
+	"errors"
+	"net/url"
 	"os/exec"
 	"runtime"
 )
 
-func OpenInBrowser(url string) error {
+func OpenInBrowser(rawURL string) error {
+	// Validate the URL to prevent command injection
+	parsedURL, err := url.Parse(rawURL)
+	if err != nil || parsedURL.Scheme == "" || parsedURL.Host == "" {
+		return errors.New("invalid URL")
+	}
+
 	var cmd string
 	var args []string
 
 	switch runtime.GOOS {
 	case "windows":
 		cmd = "cmd"
-		args = []string{"/c", "start"}
+		args = []string{"/c", "start", parsedURL.String()}
 	case "darwin":
 		cmd = "open"
+		args = []string{parsedURL.String()}
 	default: // "linux", "freebsd", "openbsd", "netbsd"
 		cmd = "xdg-open"
+		args = []string{parsedURL.String()}
 	}
-	args = append(args, url)
-
+	
+	// #nosec G204 - URL is validated above and is safe to use
 	return exec.Command(cmd, args...).Start()
 }
